29d22847bafbc0ac5656ceab8c0041ecf4ea6daa
amritach222 <amritach222@gamil.com>
2022-11-21 13:38:00 +0545

    unknown changes

--- src/feature/productCatalogue/productSlice.js
+++ src/feature/productCatalogue/productSlice.js
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 import data from "../../data";
-// To change the produt image position we write some postions to change 
+// To change the produt image position we write some postions to change
 const imagePositions = {
   left: "flex-row",
   right: "flex-row-reverse",
   top: "flex-column align-items-center",
 };
-// to chamge the product item position 
-const product_item_position={
-  'left':'flex-row-reverse',
-  'right':'flex-row',
-  'top':'flex-column-reverse align-items-center',
-  'bottom':'flex-column align-items-center'
-}
+// to chamge the product item position
+const product_item_position = {
+  left: "flex-row-reverse",
+  right: "flex-row",
+  top: "flex-column-reverse align-items-center",
+  bottom: "flex-column align-items-center",
+};
 const initialState = {
   position: imagePositions.left,
   available_color: data.catalogue.product.product_color,
   color_items: data.catalogue.product.product_color[0].images,
-  item_position:product_item_position.bottom,
-  rating:1,
-  review: ""
+  item_position: product_item_position.bottom,
+  rating: 1,
+  review: "",
 };
 export const productSlice = createSlice({
   name: "product",
@@ -37,21 +37,28 @@ export const productSlice = createSlice({
       state.color_items =
         data.catalogue.product.product_color[action.payload].images;
     },
-    changeProductItemsPosition:(state,action)=>{
+    changeProductItemsPosition: (state, action) => {
       // Call this method to change the position of  product item
-      state.item_position=product_item_position[action.payload]
+      state.item_position = product_item_position[action.payload];
     },
-    setRating:(state,action)=>{
-      // call this  method to chnage the product rating 
-      state.rating=action.payload;
+    setRating: (state, action) => {
+      // call this  method to chnage the product rating
+      state.rating = action.payload;
     },
-    writeReview:(state,action)=>{
+    writeReview: (state, action) => {
       //  call this method to write review
-      state.review=action.payload;
-      return 'success'
-    }
+      state.review = action.payload;
+      return "success";
+    },
   },
 });
 //Action Creators are generated for each case reducer function
-export const { changePosition, setColorItems, changeProductItemsPosition,setAvailableColor,setRating,writeReview} = productSlice.actions;
+export const {
+  changePosition,
+  setColorItems,
+  changeProductItemsPosition,
+  setAvailableColor,
+  setRating,
+  writeReview,
+} = productSlice.actions;
 export default productSlice.reducer;
